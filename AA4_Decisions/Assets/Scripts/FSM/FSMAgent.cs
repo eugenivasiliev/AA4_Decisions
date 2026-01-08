95225ee84e3dd707cff5d3592bc383cb99dbe1d6
Miquel Cortijo Martínez <miquel.cortijo@estudiant.enti.cat>
2026-01-08 19:05:02 +0100

    modified scene and removed comment

--- AA4_Decisions/Assets/Scripts/FSM/FSMAgent.cs
+++ AA4_Decisions/Assets/Scripts/FSM/FSMAgent.cs
@@ -20,7 +20,6 @@ public class FSMAgent : MonoBehaviour
     void Update()
     {
         stateMachine.OnUpdate();
-        //Debug.Log(stateMachine.CurrentState.ToString());
     }
 
     private void OnTriggerEnter2D(Collider2D other)
